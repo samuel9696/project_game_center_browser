36acfabd4967ba5ee79fdea28808414707611aeb
David <davidmeza@outlook.com>
2015-08-28 01:40:40 -0400

    refactor

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var modelSnake = {
 
   init: function () {
 
-    },
+  },
 
   body: [],
 
   checkForFood: function (target) {
-    if ($("#" + target).hasClass("red-bg")) {
-      $("#" + target).removeClass("red-bg")
+    if ($("#" + target).hasClass("glyphicon-apple food")) {
+      $("#" + target).removeClass("glyphicon-apple food")
       controller.placeFood();
       return true;
     } else {
@@ -20,7 +20,8 @@ var modelSnake = {
 
 var modelGrid = {
 
-  size: 20,
+  boardHeight: 20,
+  boardWidth: 30,
 
   init: function(){
     modelGrid.buildGrid()
@@ -28,26 +29,32 @@ var modelGrid = {
 
 
   buildGrid: function(){
-    var size = modelGrid.size;
-    $('#board-container').append('<table> </table>');
-    for(var i=0; i<size;i++){
-      $('table').append('<tr></tr>')
-      for(var j=1; j<size+1;j++){
-         $('tr').last().append('<td></td>')
-         $('tr td').last().attr("id",j+20*i-1)
-         if (i==0 || j==1 || i == size-1 || j == size){
-          $('tr td').last().addClass("border").text("");
-         }
+    var tableString = "<table><tbody>";
+    for(var rowPos = 0; rowPos < modelGrid.boardHeight; rowPos++) {
+      tableString += "<tr>"
+      for(var colPos = 1; colPos < modelGrid.boardWidth + 1; colPos++) {
+        var idNum = colPos + modelGrid.boardWidth * rowPos - 1;
+        if (rowPos == 0 ||
+            colPos == 1 ||
+            rowPos == modelGrid.boardHeight - 1 ||
+            colPos == modelGrid.boardWidth) {
+          tableString += ("<td id = '" + idNum.toString() + "' class = 'border'></td>");
+        } else {
+          tableString += "<td id = '" + idNum.toString() + "'></td>"
+        }
       }
+      tableString += "</tr>"
     }
+    tableString += "</tbody></table>"
+    $('#board-container').append(tableString);
   }
 
-
 }
+
 var view = {
 
   score: 0,
-
+  $score: $("#score"),
   currentDirection: null,
 
   init: function(){
@@ -57,13 +64,18 @@ var view = {
   },
 
   updateScore: function() {
-    $("#score").text(view.score)
+    console.log("Current score is " + view.score)
+    view.$score.text(view.score)
 
   }
 }
 
 
 var controller = {
+
+  timer: null,
+  isGameOver: false,
+
   init: function(){
     modelGrid.init();
     view.init();
@@ -71,92 +83,89 @@ var controller = {
   },
 
   setCallbacks: function(){
-    //$(window).on("keydown","td",controller.moveSnake);
     $(window).keydown(controller.moveSnake);
+    $(document).on("click", "#play-again", function(){
+          location.reload();
+    });
   },
 
   nextSquare: function(code){
+
     var headSquareNumber = modelSnake.body[0];
-      switch(code){
-        case 37:
-          var targetSquareNumber = headSquareNumber-1;
-          break;
+    switch(code){
+      case 37:
+      return headSquareNumber-1;
 
-        case 38:
-          var targetSquareNumber = headSquareNumber- modelGrid.size;
-          break;
+      case 38:
+      return headSquareNumber- modelGrid.boardWidth;
 
-        case 39:
-          var targetSquareNumber = headSquareNumber+1;
-          break;
+      case 39:
+      return headSquareNumber+1;
 
-        case 40:
-          var targetSquareNumber = headSquareNumber+ modelGrid.size;
-          break;
+      case 40:
+      return headSquareNumber+ modelGrid.boardWidth;
 
-        default:
-          // alert("use arrows only");
-          return console.log("wrong key");
+      default:
+      console.log("use arrows only")
+      return null;
 
-      }
-      return targetSquareNumber;
+    }
+  },
+
+  validKey: function(code) {
+    var result = $.inArray(code, [37,38,39,40])
+    if (result == -1) {
+      return false;
+    } else {
+      return true;
+    }
   },
 
   moveSnake: function(event){
 
+    if (controller.validKey(event.keyCode)) {
     view.currentDirection = event.keyCode;
     var targetSquareNumber = controller.nextSquare(view.currentDirection);
-    controller.modifySnake(targetSquareNumber);
-    view.updateScore();
+      controller.modifySnake(targetSquareNumber);
+      view.updateScore();
+    };
 
   },
 
   modifySnake: function(targetSquareNumber){
     if ($('#' + targetSquareNumber).hasClass("border") || $('#' + targetSquareNumber).hasClass("snake")){
-        controller.gameOver();
-        controller.isGameOver = true;
+      controller.gameOver();
+      controller.isGameOver = true;
+    } else {
+      var foundFood = modelSnake.checkForFood(targetSquareNumber);
+      modelSnake.body.unshift(targetSquareNumber);
+      $('#' + targetSquareNumber).addClass("snake");
+      if (!foundFood) {
+        var tail = modelSnake.body.pop();
+        $('#' + tail).removeClass("snake");
       } else {
-        var foundFood = modelSnake.checkForFood(targetSquareNumber);
-        modelSnake.body.unshift(targetSquareNumber);
-        $('#' + targetSquareNumber).addClass("snake");
-        if (!foundFood) {
-          var tail = modelSnake.body.pop();
-          $('#' + tail).removeClass("snake");
-        } else {
-          view.score +=1;
-        }
+        view.score +=1;
       }
-      controller.autoMove();
+    }
+    controller.autoMove();
   },
-
-  timer: null,
-  isGameOver: false,
 
   autoMove: function() {
     if (view.currentDirection && !controller.isGameOver){
-      var speed = 800-view.score*100;
-      if (speed < 200) speed = 200;
+      var speed = 800 - view.score * 50;
+      if (speed < 150) speed = 150;
 
       clearTimeout(controller.timer);
       controller.timer = setTimeout(function(){
         controller.modifySnake(controller.nextSquare(view.currentDirection))
       }, speed);
-      console.log(controller.timer);
     }
     console.log("it moves automaticly");
   },
 
-  // triggerMove: function() {
-  //   console.log("here");
-  //   var e = jQuery.Event("keydown");
-  //   console.log(e);
-  //   e.which = view.currentDirection;
-  //   $(window).trigger(e);
-  // },
-
   gameOver: function() {
     clearTimeout(controller.timer);
-    console.log(controller.timer);
+    $("#play-again").removeClass("hidden");
     $(window).off();
     alert("Sorry you lost!");
   },
@@ -175,8 +184,11 @@ var controller = {
       console.log(numberOfsquare);
     } while($('#' + numberOfsquare).hasClass("snake") || $('#' + numberOfsquare).hasClass("border"))
     console.log(numberOfsquare);
-    $('#' + numberOfsquare).addClass("red-bg");
+    $('#' + numberOfsquare).addClass("glyphicon-apple food");
   }
 }
 
-controller.init();
+$(function() {
+  console.log( "ready!" );
+  controller.init();
+});
